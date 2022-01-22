d318e0eb69cc57ddcfaba6f620b097c180d8bd91
lisbethchavez <chavezlisbeth639@gmail.com>
2022-01-22 18:32:40 -0500

    Agregar archivos de proyecto.

---
+++ BDDTareaLisbethChavez/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTareaLisbethChavez
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublised { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class BlogDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
